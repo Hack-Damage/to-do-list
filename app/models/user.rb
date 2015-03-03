8dbf6ab2f637e5916107ff448307eb10b03ea97a
jocelynakkurt <jocelynakkurtl@gmail.com>
2015-03-02 19:04:04 -0800

    added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :requests_sent, class_name: "Partnership", foreign_key: :friender_id, inverse_of: :friender
   has_many :requests_received, class_name: "Partnership", foreign_key: :friendee_id, inverse_of: :friendee
   has_many :comments
