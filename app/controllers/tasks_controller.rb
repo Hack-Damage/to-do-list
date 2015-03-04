10415fecdbc73049d735db4be1884ecc0a8c3fab
danielmontgomery <daniel.w.montgomery@gmail.com>
2015-03-03 18:08:15 -0800

    fixing form errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,11 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+  	task = Task.create(params.require(:name))
+  	if task.save
+  		redirect_to tasks_path
+  	end
+  end
 end
