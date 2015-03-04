0de4cdd012a43006e89b702f754bf91df495ebd2
danielmontgomery <daniel.w.montgomery@gmail.com>
2015-03-03 16:50:37 -0800

    testing addition

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-	
-	@created_user = User.create(
+	# before(:each) do
+ 
+	# end
+	it "should contain a username, email, password, and matched status"  do
+			@created_user = User.create(
           username: 'fakeuser', 
           password: "fakepassword",
           email: "joe@joe1234.com", 
           is_matched: false
-      )  
-	it "should contain a username, email, password, and matched status"  do
+      ) 
 		expect(@created_user.username).to eq("fakeuser")
-	  # expect(@created_user.email).to eq("joe@joe1234.com")
-	  # expect(@created_user.password).to eq("fakepassword")
-	  # expect(@created_user.is_matched).to eq(false)
+	  expect(@created_user.email).to eq("joe@joe1234.com")
+	  expect(@created_user.password).to eq("fakepassword")
+	  expect(@created_user.is_matched).to eq(false)
  end
 end
