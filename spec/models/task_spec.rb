eb9a4c2f1184f03c1e56acb559e9aa74d35fb9b8
danielmontgomery <daniel.w.montgomery@gmail.com>
2015-03-04 15:35:17 -0800

    fixed one styling feature, added false default value

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe Task, type: :model do
 	it "should contain a username, email, password, and matched status"  do
 		@created_task = Task.create(
           name: 'MadeTask', 
-          is_complete: "true",
           user_id: "1"
       ) 
 		expect(@created_task.name).to eq("MadeTask")
-	  expect(@created_task.is_complete).to eq(true)
+	  expect(@created_task.is_complete).to eq(false)
 	  expect(@created_task.user_id).to eq(1)
  end
 end
