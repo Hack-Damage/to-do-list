4bd8b4ca9232532112cd4432f3eddf22e03561d0
elisajohnson <elisaj33@yahoo.com>
2015-03-04 17:48:03 -0800

    broke out user tests, all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of    :email,     :case_sensitive => false, :allow_blank => true, :if => :email_changed?
   has_many :requests_sent, class_name: "Partnership", foreign_key: :friender_id, inverse_of: :friender
   has_many :requests_received, class_name: "Partnership", foreign_key: :friendee_id, inverse_of: :friendee
   has_many :comments
