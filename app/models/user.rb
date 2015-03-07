57eb17d1d303c278d0a6024add36f54e615eb369
Christopher Chandler <pherchanplist@gmail.com>
2015-03-06 18:25:48 -0800

    Add a checkbox for each task that changes is_complete to true or false after you click update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   has_many :requests_received, class_name: "Partnership", foreign_key: :friendee_id, inverse_of: :friendee
   has_many :comments
   has_many :tasks
+  accepts_nested_attributes_for :tasks
 end
