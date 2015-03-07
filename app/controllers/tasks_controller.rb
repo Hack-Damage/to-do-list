d8502e0f123b9fe89483c4ca2e9733c4b5906fa8
Christopher Chandler <pherchanplist@gmail.com>
2015-03-06 17:08:36 -0800

    Task created is associated with current user logged in and that task displays for them on their index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,9 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    #assign an instance variable to the current user tasks so we can list these
+    #on the tasks page for the user that is signed in
+    @current_user_tasks = Task.where(user_id:current_user)
   end
 
   def edit
@@ -11,7 +14,8 @@ class TasksController < ApplicationController
   end
 
   def create
-  	@task = Task.new(task_params)
+  	@task = current_user.tasks.new(task_params)
+    #@task.user_id = current_user.user_id if current_user
   	if @task.save
   		redirect_to tasks_path
     else
@@ -22,6 +26,6 @@ class TasksController < ApplicationController
 private
   def task_params
       params.require(:task).permit(
-        :name)
+        :name, :user_id)
   end
 end
