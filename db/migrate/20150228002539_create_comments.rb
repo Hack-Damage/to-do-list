018866b8480b70dfef3ef25d77b9d4899b0cda84
Christopher Chandler <pherchanplist@gmail.com>
2015-03-11 15:16:57 -0700

    add partnership tables

--- db/migrate/20150228002539_create_comments.rb
+++ db/migrate/20150228002539_create_comments.rb
@@ -3,11 +3,9 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :title
       t.references :user, index: true
-      t.references :partnership, index: true
 
       t.timestamps null: false
     end
     add_foreign_key :comments, :users
-    add_foreign_key :comments, :partnerships
   end
 end
--- spec/factories/partnerships.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :partnership do
-    
-  end
-
-end
