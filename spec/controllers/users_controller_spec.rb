167af022534cd4313bff6cb772f059fda07bac4d
jocelynakkurt <jocelynakkurtl@gmail.com>
2015-03-12 17:24:54 -0700

    additional conttroller testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,4 +8,41 @@ RSpec.describe UsersController, type: :controller do
 		expect(response).to have_http_status(:success)
 		end
 	end
+
+
+  	describe "GET #index" do
+    	it "returns http success" do
+      	get :index
+      	expect(response).to have_http_status(:success)
+    end
+  end
+	 
+	describe "GET #new" do
+    	it "returns http success" do
+      	get :new
+      	expect(response).to have_http_status(:success)
+    end
+  end	
+	describe 'GET #index' do
+    	context 'when user is logged in' do
+    	it 'renders the Task page'
+  	end
+    	context 'when user is logged out' do
+    	it 'redirects to welcome page'
+  	end
+end
+
+	describe UsersController do 
+ 	 	it "GET #index show a list of user" do
+    	User.create(email: 'jocelynakkurt@gmail.com', is_matched: 'nill')
+    
+ 	 	end
+	end
+
+    describe 'a user without an email' do
+      before do
+        post :create
+      end
+
+ 	end
 end
